ba3ba1a50147887b181f3346e58661aa20e07aa1
Peechaya <bluewatermelon@free.fr>
2017-09-19 11:34:51 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+  <title>MadMouse Effect</title>
+
+  <!-- Bootstrap -->
+  <link href="assets/css/style.css" rel="stylesheet">
+  <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+</head>
+<body>
+
+
+
+  <header>
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="#"><img src="assets/img/logo-madmouse.png"></a>
+        </div>
+
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="#showreel">Showreel</a></li>
+          <li><a href="#competences">Compétences</a></li>
+          <li><a href="#portfolio">Portfolio</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </div>
+    </nav>
+
+<div class="gap-large"></div>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-md-5 text-right"><img src="assets/img/logo-home-madmouse.png"></div>
+        <div class="col-md-7">
+          <h1>MadMouse Effect</h1>
+          <h2>Graphiste Motion Designer</h2>
+        </div>
+      </div>
+
+      <div class="gap"></div>
+
+      <div class="text-center">
+        <iframe src="https://player.vimeo.com/video/225380697" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+      </div>
+    </div>
+
+  </header>
+
+  <section id="competences">
+    <div class="container">
+      <h3>Compétences</h3>
+      <hr>
+
+      <div class="row">
+        <div class="col-md-4">
+          <h4><span class="glyphicon glyphicon-user"></span> Logos & Mascottes </h4>
+          <p>Moyen efficace pour communiquer avec votre audience sur votre entreprise, votre site Web ou vos applications mobiles...</p>
+        </div>
+
+        <div class="col-md-4">
+          <h4><span class="glyphicon glyphicon-user"></span> Logos & Mascottes </h4>
+          <p>Moyen efficace pour communiquer avec votre audience sur votre entreprise, votre site Web ou vos applications mobiles...</p>
+        </div>
+
+        <div class="col-md-4">
+          <h4><span class="glyphicon glyphicon-user"></span> Logos & Mascottes </h4>
+          <p>Moyen efficace pour communiquer avec votre audience sur votre entreprise, votre site Web ou vos applications mobiles...</p>
+        </div>
+      </div>
+
+      <p class="text-center"><input type="button" value="Télécharger mon CV"></input></p>
+    </div>
+  </section>
+
+  <section id="portfolio">
+    <h3>Portfolio</h3>
+    <hr>
+
+  </section>
+
+  <section id="contact">
+      <div class="container">
+        <h3>Contact</h3>
+        <hr>
+
+        <p class="text-center"><a href=""><span class="glyphicon glyphicon-user"></span></a> <a href=""><span class="glyphicon glyphicon-user"></span></a> <a href=""><span class="glyphicon glyphicon-user"></span></a></p>
+
+
+        <form class="contact text-center">
+  <div class="form-group">
+    <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Nom">
+  </div>
+  <div class="form-group">
+    <input type="email" class="form-control" id="exampleInputPassword1" placeholder="Email">
+  </div>
+  <div class="form-group">
+    <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Sujet">
+  </div>
+  <div class="form-group">
+      <textarea class="form-control" rows="5" placeholder="Message"></textarea>
+  </div>
+
+  <button type="submit" class="btn btn-default">Envoyer</button>
+</form>
+
+      </div>
+  </section>
+
+  <footer>
+      <p class="text-center">© 2017 by Bensafidine Walid. Alias MadMouse Effect.</p>
+  </footer>
+
+  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <!-- Include all compiled plugins (below), or include individual files as needed -->
+  <script src="assets/js/bootstrap.min.js"></script>
+</body>
+</html>
