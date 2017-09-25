18a09f47d16b64f3e2f8177f407b9d0d61d3c22e
Peechaya <bluewatermelon@free.fr>
2017-09-25 20:33:42 +0200

    CSS on progress

--- index.php
+++ index.php
@@ -24,9 +24,14 @@
   <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 
-<script>
-
-</script>
+  <script>
+  $(function () {
+    $(document).scroll(function () {
+  	  var $nav = $(".navbar-inverse");
+  	  $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+  	});
+  });
+  </script>
 
 </head>
 <body>
@@ -176,6 +181,8 @@
 
 
   <!-- Include all compiled plugins (below), or include individual files as needed -->
+
+
   <script src="assets/js/bootstrap.min.js"></script>
   <script src="assets/js/portfolio.js"></script>
 </body>
